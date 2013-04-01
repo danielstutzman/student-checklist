d0fe9350bac7a5bd6160682331942f6f531317c2
Daniel Stutzman <dtstutz@gmail.com>
2013-04-01 11:14:45 -0600

    Show attempts as divs inside task div.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -19,11 +19,21 @@
 
     for (task_id in task_id_to_initials_to_attempt) {
       var initials_to_attempt = task_id_to_initials_to_attempt[task_id];
+      var $div = $('#task-' + task_id);
+      var html = '';
       for (var i = 0; i < all_initials.length; i++) {
         var initials = all_initials[i];
         var attempt = initials_to_attempt[initials];
-        console.log(attempt);
+        if (attempt === undefined) {
+          attempt = { 'completed' : false };
+        }
+        var class_ = 'attempt ';
+        class_ += attempt.completed ? 'completed ' : 'incomplete ';
+        var x_or_not = attempt.completed ? 'X' : '-';
+        html += "<div id='task-" + task_id + "-" + initials + "' class='" +
+          class_ + "'>" + x_or_not + "</div>";
       }
+      $div.html(html);
     }
   });
 })();
